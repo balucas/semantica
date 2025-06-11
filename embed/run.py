9cf6a9f9d76db0923c9a39490e87939f01462455
ManueLucas <m.lucas2603@gmail.com>
2025-06-10 22:04:09 -0400

    embed uses port defined in docker compose

--- embed/run.py
+++ embed/run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from os import environ
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5001)))
