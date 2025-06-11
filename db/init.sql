000f69a518fdf8af863b44ba053cf8f38bb6161b
ManueLucas <m.lucas2603@gmail.com>
2025-06-10 20:13:21 -0400

    updated schema for note pivot

--- db/init.sql
+++ db/init.sql
@@ -1,29 +1,37 @@
 CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
 
-CREATE TABLE user_text 
+CREATE TABLE note_content 
 (
-id bigserial PRIMARY KEY, 
-user_id integer NOT NULL, 
-context_reference bigserial
-embedding vector(3),
-date_added timestamp,
-actual_data text NOT NULL
+    id bigserial PRIMARY KEY REFERENCES metadata_and_context(id) ON DELETE CASCADE, 
+    content text,
 );
 
-CREATE TABLE metadata_and_context
+CREATE TABLE note_metadata
 (
-id bigserial PRIMARY KEY,
-date_added timestamp,
-site_url text,
-context_descriptor text NOT NULL,
-embedding vector(3)
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    content_id bigserial
+    user_id integer NOT NULL, 
+    date_added timestamp,
+    date_updated timestamp,
+    -- site_url text,
+    -- context_descriptor text,
+    content_format text DEFAULT 'markdown'
+    embedding vector(512)
+);
 
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    username text UNIQUE,
+    password_salt VARCHAR(255),
+    password_hash VARCHAR(255)
 )
-CREATE TABLE user_bigtext
-(
-id bigserial PRIMARY KEY, 
-user_id integer NOT NULL, 
-embedding vector(3),
-date_added timestamp,
-datapath text NOT NULL
-);
+-- CREATE TABLE user_bigtext
+-- (
+-- id bigserial PRIMARY KEY, 
+-- user_id integer NOT NULL, 
+-- embedding vector(3),
+-- date_added timestamp,
+-- datapath text NOT NULL
+-- );
